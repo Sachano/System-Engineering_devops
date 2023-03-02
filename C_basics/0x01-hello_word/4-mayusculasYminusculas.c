e0997d27622f9e967d35f0dd0243539cf4fafa8a
Sachano <125321469+Sachano@users.noreply.github.com>
2023-03-02 05:43:20 -0400

    Add files via upload

---
+++ C_basics/0x01-hello_word/4-mayusculasYminusculas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int lw = 'a';
+int up = 'A';
+
+while (lw <= 'z')
+{
+putchar(lw);
+lw += 1;
+}
+
+while (up <= 'Z')
+{
+putchar(up);
+up += 1;
+}
+putchar('\n');
+
+return (0);
+}
---
+++ C_basics/0x01-hello_word/7-putchar.c
@@ -0,0 +1,16 @@
+	#include <stdio.h>
+	
+	int main(void)
+	{
+	
+	int n = 48;
+	
+		while (n <= 57)
+			{
+			putchar(n);
+			n += 1;
+			}
+	printf("\n");
+	
+    return (0);
+	}
